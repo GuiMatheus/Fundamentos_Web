7bdd815d1c518136c331c5dc43ddc8b403df488e
Gui Matheus <guilherme.maths.sil@gmail.com>
2021-08-19 14:59:55 -0300

    adicionando itens de java script

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+
+
+
+
+let  nome = window.document.getElementById('nome')
+let email = window.document.querySelector('#email')
+let assunto = window.document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let asstunoOk = false
+let mapa = document.querySelector('#mapa')
+
+
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome (){
+    let txtNome = document.querySelector('#txtNome')
+ if (nome.value.length < 3){
+    txtNome.innerHTML = 'Nome Invalido'
+    txtNome.style.color = 'red'
+ }else{
+    txtNome.innerHTML = 'Nome Valido'
+    txtNome.style.color = 'green'
+    nomeOk = true
+ }
+}
+
+function validaEmail (){
+    let txtEmail = document.querySelector('#txtEmail')
+ if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ){
+    txtEmail.innerHTML = 'Email Invalido'
+    txtEmail.style.color = 'red'
+ }else{
+     txtEmail.innerHTML = 'Email Valido'
+     txtEmail.style.color = 'green'
+     emailOk = true
+ }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if (assunto.value.length >= 100){
+        txtAssunto.innerHTML = 'Texto muito grande, por favor digite no maximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+     }else{
+         
+         txtAssunto.style.display = 'none'
+         assuntoOk = true
+     }
+}
+
+
+function enviar(){
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Formulario enviado com sucesso')
+    } else {
+        alert('Preencha corretamente antes de enviar')
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '800px'
+    mapa.style.heigth = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '400px'
+    mapa.style.heigth = '300px'
+}
